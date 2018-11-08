2f367bb8e4e7ce1e09f1f72cb014f6ca7348c2c8
Josh <joshbgosh10592@gmail.com>
2018-11-08 01:27:05 -0500

    Added ability to create the trigger file (DeleteMeWhenReady.txt)

--- LAMEConverter.ps1
+++ LAMEConverter.ps1
@@ -1,5 +1,6 @@
 ﻿#LAME audio converter that converts basically any audio to MP3 format.
 #More info on LAME commands can be found here: http://ecmc.rochester.edu/ecmc/docs/lame/switchs.html
+
 $WorkingDir = "S:\Upload\Services"
 $InputPath = "$WorkingDir\Input"
 $OutputPath = "$WorkingDir\Output"
@@ -9,25 +10,38 @@ $InputFiles = Get-ChildItem -Path $InputPath -Recurse -Include *.wav | select -e
 $BitRate = "96"
 Function WavToMP3Convert
 {
-Write-Host "This is inside the WavToMP3Convert Function" -ForegroundColor Yellow
-    Param ($InputPath, $OutputPath, $OutputFile, $LAMEexe, $LogFile, $InputFiles, $CurrentDate)
+    <#Param (
+    $InputPath, 
+    $OutputPath, 
+    $OutputFile, 
+    $LAMEexe, 
+    $LogFile, 
+    $InputFiles, 
+    $CurrentDate
+    )#>
+    Write-Host "This is inside the WavToMP3Convert Function" -ForegroundColor Yellow
     Foreach ($InputFile in $InputFiles)
     {
         Write-Host $InputFile
         Write-Host $OutputPath
         $OutputFile = $InputFile.split('\.')[-2]
             & $LAMEexe $InputFile $OutputPath\$OutputFile.mp3 -b $BitRate
-        LogWrite ($LogFile, $CurrentDate, $InputFile)
+        LogWrite $LogFile $CurrentDate $InputFile $OutputFile
     }
 }
 Function LogWrite
 {
+  <#Param (
+   $LogFile, 
+   $CurrentDate, 
+   $OutputFile)#>
    BuildDates
    Add-content -Path $Logfile -value "On $CurrentDate, file $OutputFile was converted."
 }
-Function BuildDates ($LogFile)
+Function BuildDates
 {
-    $CurrentDate = Get-Date
-    $CurrentDate = $CurrentDate.ToString('MM-dd-yyyy_hh-mm-ss')
+    $script:CurrentDate = Get-Date
+    $script:CurrentDate = $CurrentDate.ToString('MM-dd-yyyy_hh-mm-ss')
 }
 WavToMP3Convert
+New-Item -ItemType "file" -Path $InputPath\DeleteMeWhenReady.txt
