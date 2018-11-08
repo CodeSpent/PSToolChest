dc17954e463b5ee4f7d7b64adc7032ea515c5e2f
Josh <joshbgosh10592@gmail.com>
2018-11-07 23:20:47 -0500

    First Commit

---
+++ LAMEConverter.ps1
@@ -0,0 +1,13 @@
+﻿#LAME audio convert for 
+$InputPath = "S:\Upload\Services\Input"
+$OutputPath = "S:\Upload\Services\Output"
+$LAMEexe = "C:\Program Files (Personal)\lame3.100-64\lame.exe"
+#[array]$InputFiles = Get-ChildItem $InputPath | Where {$_.extension -like ".wav"} | select -expand name
+$InputFiles = Get-ChildItem -Path $InputPath -Recurse -Include *.wav | select -expand fullname
+Foreach ($InputFile in $InputFiles)
+    {
+    Write-Host $InputFile
+    Write-Host $OutputPath
+    $OutputFile = $InputFile.split('\.')[-2]
+        & $LAMEexe $InputFile $OutputPath\$OutputFile.mp3
+    }
