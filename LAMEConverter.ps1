8e26702bf103c8fa7ee262ceb6e6483a855a143b
Josh <joshbgosh10592@gmail.com>
2018-11-07 23:42:49 -0500

    Attempting to add logging

--- LAMEConverter.ps1
+++ LAMEConverter.ps1
@@ -2,12 +2,33 @@
 $InputPath = "S:\Upload\Services\Input"
 $OutputPath = "S:\Upload\Services\Output"
 $LAMEexe = "C:\Program Files (Personal)\lame3.100-64\lame.exe"
-#[array]$InputFiles = Get-ChildItem $InputPath | Where {$_.extension -like ".wav"} | select -expand name
+$LogFile = "$OutputPath\wavToMP3.log"
 $InputFiles = Get-ChildItem -Path $InputPath -Recurse -Include *.wav | select -expand fullname
-Foreach ($InputFile in $InputFiles)
+Function WavToMP3Convert
     {
-    Write-Host $InputFile
-    Write-Host $OutputPath
-    $OutputFile = $InputFile.split('\.')[-2]
-        & $LAMEexe $InputFile $OutputPath\$OutputFile.mp3
+    Param ($InputPath, $OutputPath, $OutputFile, $LAMEexe, $LogFile, $InputFiles, $CurrentDate)
+    Foreach ($InputFile in $InputFiles)
+        {
+        Write-Host $InputFile
+        Write-Host $OutputPath
+        $OutputFile = $InputFile.split('\.')[-2]
+            & $LAMEexe $InputFile $OutputPath\$OutputFile.mp3
+        LogWrite
+        }
     }
+Function LogWrite
+{
+   Param ($LogFile, $CurrentDate, $InputFile)
+   BuildDates
+   Write-Host "BuildDates Function Being called" -ForegroundColor Yellow
+   Add-content $Logfile -value "On $CurrentDate, file $InputFile was converted."
+}
+Function BuildDates
+{
+    Write-Host
+    $CurrentDate = Get-Date
+    $CurrentDate = $CurrentDate.ToString('MM-dd-yyyy_hh-mm-ss')
+}
+Write-Host "Starting" -ForegroundColor Yellow
+WavToMP3Convert
+Write-Host "WavToMP3Convert was called" -ForegroundColor Yellow
