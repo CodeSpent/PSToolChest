5f331441f95629f7dc444d604bb04e1816ee2cf2
Joshbgosh10592 <Joshbgosh10592@gmail.com>
2018-11-30 16:20:20 -0500

    Added group additions during creation, better error handling, and specific desc for service accounts

--- ActiveDirectory/ADNewUser.ps1
+++ ActiveDirectory/ADNewUser.ps1
@@ -22,13 +22,14 @@ $OUDomain = "$DomainName" + "Users"
 $Groups = Read-Host "Enter any groups, seperated by coma space. Leave empty if none"
 
 # Prompt User for Type
-$input =Read-Host "What Type of account? Admin-(1), Dev- (2), SRV- (2)"
+$input =Read-Host "What Type of account? Admin-(1), Dev- (2), SRV- (3)"
 switch ($input)
     {
     '1'{
         $Type = "admin"
         # Create the full OU Path
         $OU = "OU=Administration,OU=$OUDomain,DC=$DomainName,DC=$TLDN"
+        $Description = "$Type account for $FLName"
         # Add normal account to extra Group
         ######Need to decide how to sanitize
         }
@@ -36,21 +37,30 @@ switch ($input)
         $Type = "dev"
         # Create the full OU Path
         $OU = "OU=Administration,OU=$OUDomain,DC=$DomainName,DC=$TLDN"
+        $Description = "$Type account for $FLName"
         }
     '3'{
         $Type = "srv"
         # Create the full OU Path
         $OU = "OU=Service Accounts,OU=EnterpriseGroups,DC=$DomainName,DC=$TLDN"
+        $srvOwner = Read-Host "What is the Owner name?"
+        $srvOwnerDept = Read-host "What is the Owner Department?"
+        $srvPurpose = Read-Host "What is the Purpose?"
+        $Description = "Purpose: $srvPurpose. OwnerDept: $srvOwnerDept. Owner: $srvOwner"
         }
     }
 # Do the work
 Try{
-New-ADUser -Name $NewUser -SamAccountName $NewUser -UserPrincipalName $UPN -Path $OU -Description "$Type account for $FLName" -AccountPassword(Read-Host -AsSecureString "Type Password for $NewUser") -DisplayName $NewUser -Enabled $True
+    New-ADUser -Name $NewUser -SamAccountName $NewUser -UserPrincipalName $UPN -Path $OU -Description $Description -AccountPassword(Read-Host -AsSecureString "Type Password for $NewUser") -DisplayName $NewUser -Enabled $True -ErrorAction Stop
 }
+Catch [Microsoft.ActiveDirectory.Management.ADIdentityAlreadyExistsException]
+    {
+    Write-Host "$NewUser already exists" -ForegroundColor Green -BackgroundColor Red
+    }
 Catch{
     If ($Error.Exception -like '*The operation failed because UPN value provided for addition*is not unique forest-wide*')
         {
-        Write-Host "$NewUser already exists" -ForegroundColor Green -BackgroundColor Red
+        Write-Host "$NewUser already exists (UPN not unique)" -ForegroundColor Green -BackgroundColor Red
         }
     Else
         {
@@ -62,5 +72,6 @@ If ($Groups)
     Foreach ($Group in $Groups.Split(", "))
         {
         Add-ADGroupMember -Identity $Group -Members $NewUser
+        Write-Host "Added $NewUser to $Group"
         }
     }
