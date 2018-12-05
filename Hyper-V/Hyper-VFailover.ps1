8d35fc255bf6454f0cf9d658649af71e05ad8bfa
Joshbgosh10592 <Joshbgosh10592@gmail.com>
2018-12-05 18:54:24 -0500

    Working!

--- Hyper-V/Hyper-VFailover.ps1
+++ Hyper-V/Hyper-VFailover.ps1
@@ -1,4 +1,6 @@
-﻿$WorkingDir = "D:\Scripts"
+﻿$VerbosePreference = "Continue"
+$VerbosePreference
+$WorkingDir = "D:\Scripts"
 $SourceFile = "$WorkingDir\SourceFiles\Hyper-V_Failover.txt"
 $ResultsDir = "$WorkingDir\Results\"
 #Read-Host "This will read from $SourceFile. Press ENTER when ready"
@@ -17,12 +19,11 @@ $CurrentDate = Get-Date
 $CurrentDate = $CurrentDate.ToString('MM-dd-yyyy_hh-mm-ss')
 $ResultsFile = -join ("$ResultsDir", "Hyper-VFailover", "_", "$CurrentDate.csv")
 Function WritePropsToCSV {
-    Param ($HyperVHost, $HyperVHostPri, $HyperVHostRep, $RunningVMsName, $Repeater, $FailoverStatus, $Notes)
+    Param ($HyperVHost, $HyperVHostPri, $HyperVHostRep, $RunningVMName, $FailoverStatus, $Notes)
         $Props = [ordered]@{
             "HyperVHostPri" = "$HyperVHostPri";
             "HyperVHostRep" = "$HyperVHostRep";
-            "VMName" = "$RunningVMsName";
-            "Repeater" = "$Repeater"
+            "VMName" = "$RunningVMName";
             "Status" = "$FailoverStatus"
             "Notes" = "$Notes"
             }#end props
@@ -31,12 +32,12 @@ Function WritePropsToCSV {
     }
 Function FailOverVM {
     Param($RunningVMName, $HyperVHost)
-    # Perform Failover on RunningVMsName
+    # Perform Failover on RunningVMNames
     Write-Host $RunningVMName | Format-List
     $HyperVHostRep = Get-VMReplication -ComputerName $HyperVHost -VMName $RunningVMName
     $HyperVHostRep = $HyperVHostRep.ReplicaServer
     Stop-VM -ComputerName $HyperVHost -Name $RunningVMName -Force
-    #while(!($RunningVMsName.State -like "Off")) {
+    #while(!($RunningVMNames.State -like "Off")) {
     #    Write-Host "Waiting for Off"
     #    }
     Start-VMFailover -Prepare -VMName $RunningVMName -ComputerName $HyperVHostPri -Confirm:$false -ErrorAction Stop
@@ -46,20 +47,23 @@ Function FailOverVM {
     Write-Host "$RunningVMName was successfully failed over"
     # Write Props to CSV
     $FailoverStatus = "Success"
-    WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $Repeater $FailoverStatus $Notes
+    WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $FailoverStatus $Notes
 }
 Foreach ($HyperVHost in $HyperVHosts) {
     $HyperVHostPri = $HyperVHost
+    Write-Host "HyperVHostPri: $HyperVHostPri"
     If(Test-Connection -ComputerName $HyperVHost -Quiet -Count 1) {
+        Write-Host "Test-Connect success with $HyperVHost"
         Try {
             # Get VM list from Host
             # This is in a Try block because if the Hyper-V Management service isn't running on the host
             # then we want to capture that and place it into our log csv.
             $VMs = Get-VM -ComputerName $HyperVHost -ErrorAction Stop
-            Try {# RunningVMsName steps
+            Write-Host "$VMs"
+            Try {# RunningVMNames steps
                 $RunningVMs = $VMs | Where-Object {$_.State -like "Running"}
-                $RunningVMsName = $RunningVMs.Name
-                If ($RunningVMsName) {
+                $RunningVMNames = $RunningVMs.Name
+                If ($RunningVMNames) {
                     Foreach ($RunningVMName in $RunningVMNames) {
                         Try {
                             FailOverVM $RunningVMName $HyperVHost
@@ -70,34 +74,34 @@ Foreach ($HyperVHost in $HyperVHosts) {
                             $FailoverStatus = "Something Failed during the failover process of $HyperVHostPri with $RunningVMName."
                             $Notes = $Error[0].Exception
                             # Write Props to CSV
-                            WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $Repeater $FailoverStatus $Notes
+                            WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $FailoverStatus $Notes
                             }
                         }
                     }
-                else {
-                    Write-Host "$RunningVMsName Is null for $HyperVHost"
+                Else {
+                    Write-Host "$RunningVMNames is null for $HyperVHost"
                     $FailoverStatus = "$HyperVHost has no VMs running"
                     $Notes = "$Error[0]"
-                }
+                    }
                 }
             Catch {
-                Write-Host "Something failed while getting $PrRunningVMsName from $HyperVHost"
-                $FailoverStatus = "Couldn't get $RunningVMName from $HyperVHost"
+                Write-Host "Something failed while getting RunningVMs from $HyperVHost"
+                $FailoverStatus = "Couldn't get Running VMs from $HyperVHost"
                 $Notes = "$Error[0]"
-                WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $Repeater $FailoverStatus $Notes
+                WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $FailoverStatus $Notes
                 }
             }
-        Catch{
+        Catch {
             Write-Host "Couldn't get VM List from $HyperVHost."
             $FailoverStatus = "Failed to retrieve VM list from $HyperVHost"
             $Notes = "$Error[0]"
-            WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $Repeater $FailoverStatus $Notes
+            WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $FailoverStatus $Notes
             }
-    Else{
+        }
+    Else {
         Write-Host "Test-Connection failed"
         $FailoverStatus = "Test-Connection failed. Offline"
         $Notes = "$Error[0]"
-        WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $Repeater $FailoverStatus $Notes      
+        WritePropsToCSV $HyperVHost $HyperVHostPri $HyperVHostRep $RunningVMName $FailoverStatus $Notes      
         }
     }
-}
